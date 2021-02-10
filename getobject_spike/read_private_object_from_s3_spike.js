213c437e411cc913da2a4536e45c8de48566bc06
Brian Button <bbutton@agilestl.com>
2021-02-10 13:37:41 -0600

    testing out on server

--- getobject_spike/read_private_object_from_s3_spike.js
+++ getobject_spike/read_private_object_from_s3_spike.js
@@ -1,5 +1,4 @@
 const { S3Client, GetObjectCommand } = require("@aws-sdk/client-s3");
-const { STSClient, AssumeRoleCommand } = require("@aws-sdk/client-sts");
 const fs = require('fs');
 
 // Specify the AWS Region, such as "us-east-1".
@@ -7,42 +6,35 @@ const REGION = "us-east-2";
 
 // Set the parameters.
 // Specify the name of the bucket and the object to return.
-const stsParams = {
-    DurationSeconds: 900,
-    RoleArn: "arn:aws:iam::249332642842:role/TSBMainBucketReadOnlyAccessRole",
-    RoleSessionName: "GetBucketContentsTempSession"
-};
 
 // Create an Amazon S3 client service object.
 
-const run = async () => {
+const run = async (bucket, object) => {
     try {
-        const stsClient = new STSClient({ region: REGION});
-        const stsData = await stsClient.send(new AssumeRoleCommand(stsParams));
-        const accessParams =  {
-            accessKeyId: stsData.Credentials.AccessKeyId,
-            secretAccessKey: data.Credentials.SecretAccessKey,
-            sessionToken: stsData.Credentials.SessionToken
-        };
-
-        const s3ClientParams = {
-            region: REGION,
-            accessKeyId: stsData.Credentials.AccessKeyId,
-            secretAccessKey: stsData.Credentials.SecretAccessKey,
-            sessionToken: stsData.Credentials.SessionToken
+         const s3ClientParams = {
+            region: REGION
         }
         const s3GetObjectParams = {
-            Bucket: "thestudybar-initialspike",
-            Key: "bbutton-headshot-850x850.png",
+            Bucket: bucket,
+            Key: object
         };
         const s3 = new S3Client(s3ClientParams);
         const data = await s3.send(new GetObjectCommand(s3GetObjectParams));
-        //console.log("Success, bucket returned", data);
+        console.log("Success, bucket returned", data);
         const outputWriter = process.stdout;
         await data.Body.pipe(outputWriter);
     } catch (err) {
         console.log("Error", err);
     }
 };
-run();
 
+function main() {
+    if (process.argv.length != 4) {
+        console.log("usage: node " + process.argv[1] + " <bucket> <file>");
+        process.exit(1);
+    }
+
+    run(process.argv[2], process.argv[3]);
+}
+
+main();
